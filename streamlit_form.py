ab49aeb98ff60746f517c12e7c94fb1d2aeb6776
Yajendra_Prajapati <86663172+Yajendra2201@users.noreply.github.com>
2022-11-14 21:58:55 +0530

    new one

--- streamlit_form.py
+++ streamlit_form.py
@@ -1,35 +1,51 @@
 import streamlit as st
 st.header("Naya Naya Form")
+with st.form("My Form"):
+    Firstname = st.text_input("Enter your first name:-")
+    Lastname = st.text_input("Enter your last name:-")
+    Email = st.text_input("Enter your email address:-")
 
-Firstname = st.text_input("Enter your first name:-")
-Lastname = st.text_input("Enter your last name:-")
-Email = st.text_input("Enter your email address:-")
+    upload = st.file_uploader("Upload your image:-")
 
-upload = st.file_uploader("Upload your image:-")
+    Gender = st.selectbox('Gender',('Male','Female'))
 
-Gender = st.selectbox('Gender',('Male','Female'))
+    st.subheader('Apne baare me tick kijiye')
 
-st.subheader('Apne baare me tick kijiye')
+    pagal = st.checkbox('Pagal')
+    bewakoof = st.checkbox('Bewakoof')
+    chutiya = st.checkbox('Chutiya')
 
-pagal = st.checkbox('Pagal')
-bewakoof = st.checkbox('Bewakoof')
-chutiya = st.checkbox('Chutiya')
+    if pagal:
+        st.write("Great You are a certified paagal 😆")
 
-if pagal:
-     st.write("Great You are a certified paagal 😆")
+    if bewakoof: 
+        st.write("Kya bat hai aap to bewakoof nikale 😆")
 
-if bewakoof: 
-     st.write("Kya bat hai aap to bewakoof nikale 😆")
+    if chutiya:
+        st.write("Humko to malum tha ki aap chutiye ho 😆 ")
 
-if chutiya:
-     st.write("Humko to malum tha ki aap chutiye ho 😆 ")
+    st.subheader("Ye solve karke batao to mane")
+    st.latex(r'''
+        a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+        \sum_{k=0}^{n-1} ar^k =
+        a \left(\frac{1-r^{n}}{1-r}\right)
+        ''')
 
-st.subheader("Ye solve karke batao to mane")
-st.latex(r'''
-     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-     \sum_{k=0}^{n-1} ar^k =
-     a \left(\frac{1-r^{n}}{1-r}\right)
-     ''')
+    submitted = st.form_submit_button('Submit')
+
+if submitted:
+    st.markdown(f'''
+         You have Filled:
+        - First Name: `{Firstname}`
+        - Last Name: `{Lastname}`
+        - Email: `{Email}`
+        - File uploaded name: `{upload}`
+        - Gender: `{Gender}`
+        
+        ''')
+else:
+    st.write('Firse bharo beta')
+
 
      
 
